64cc7fbbf141236a791a5dc50c182b6aad95e7f7
Giovanny Guillen <giovanny_guillen_b@hotmail.com>
2022-10-26 12:21:55 -0500

    first commit

---
+++ js/showup.js
@@ -0,0 +1,18 @@
+function showModal()
+{
+  $('#modal').show();
+}
+
+function hideModal()
+{
+  $('#modal').hide();
+}
+function showModal2()
+{
+  $('#modal2').show();
+}
+
+function hideModal2()
+{
+  $('#modal2').hide();
+}
